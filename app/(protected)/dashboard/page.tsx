0ebbd33d8a77c248ada0e90307a4f0ade6f0af81
kishore kumar <kishornaveen2193@GMAIL.COM>
2025-06-24 15:01:16 +0530

    Update page.tsx

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -3,21 +3,26 @@ import connectDB from "@/lib/db";
 import User from "@/lib/model/User";
 import Dashboard from "@/components/Dashboard";
 import { getUserReminders } from "@/lib/actions/getReminders";
+import { redirect } from "next/navigation"; 
 
 export default async function DashboardPage() {
   await connectDB();
 
   const clerkUser = await currentUser();
-  console.log(clerkUser)
-  if (!clerkUser?.id) return <div className="text-center mt-10">User not found</div>;
+  if (!clerkUser?.id) {
+    return <div className="text-center mt-10">User not found</div>;
+  }
 
   const dbUser = await User.findOne({ clerkId: clerkUser.id });
-  if (!dbUser) return <div className="text-center mt-10">User not in DB</div>;
+
+  
+  if (!dbUser) {
+    redirect("/auth-callback");
+  }
 
   const reminders = await getUserReminders();
 
   const plainUser = JSON.parse(JSON.stringify(dbUser));
-  console.log(plainUser)
   const plainReminders = JSON.parse(JSON.stringify(reminders));
 
   return <Dashboard user={plainUser} initialReminders={plainReminders} />;
